40007f821abba02bb653f954d11e3dc2b2b4befe
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-04-23 20:09:21 +0800

    feat: Lifecycle method

--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -4,42 +4,26 @@ class ZipForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      zipcode: ''
-    };
-
-    this.inputUpdated = this.inputUpdated.bind(this);
-    this.submitZipcode = this.submitZipcode.bind(this);
+    this.submitZipCode = this.submitZipCode.bind(this);
   }
 
-  submitZipcode(e) {
-    e.preventDefault();
-
-    const { zipcode } = this.state;
+  submitZipCode(e) {
     const { onSubmit } = this.props;
 
-    onSubmit(zipcode);
-    this.setState({ zipcode: '' });
-  }
-
-  inputUpdated(e) {
-    const { value } = e.target;
-
-    this.setState({ zipcode: value });
+    onSubmit(e.target.value);
   }
 
   render() {
     return (
       <div className="zip-form">
-        <form onSubmit={this.submitZipcode}>
+        <form>
           <label htmlFor="zipcode">Zip Code</label>
-          <input
-            className="form-control"
-            type="input"
-            name="zipcode"
-            value={this.state.zipcode}
-            onInput={this.inputUpdated} />
-          <button type="submit" className='btn btn-success'>Get the forecast!!</button>
+          <select onChange={this.submitZipCode}>
+            <option value="">Select a zip</option>
+            {this.props.zips.map(zip =>
+              <option key={zip} value={zip}>{zip}</option>
+            )}
+          </select>
         </form>
       </div>
     );
